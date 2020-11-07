a78b5d648634a0752cd6a07776ac09c5a8c88d48
quavious <leenowon97@gmail.com>
2020-11-07 23:48:22 +0900

    Solution One

--- main.go
+++ main.go
@@ -2,11 +2,9 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
+	"strings"
 	"text/template"
-
-	"github.com/julienschmidt/httprouter"
 )
 
 var tpl *template.Template
@@ -15,61 +13,36 @@ func init() {
 	tpl = template.Must(template.ParseGlob("templates/*"))
 }
 
-func handleError(w http.ResponseWriter, e error) {
-	if e != nil {
-		http.Error(w, e.Error(), http.StatusInternalServerError)
-		log.Println(e)
-	}
-}
-
-func user(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-	fmt.Fprintf(w, "User, %s!\n", ps.ByName("name"))
-}
-
-func blogRead(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-	fmt.Fprintf(w, "READ CATEGORY, %s!\n", ps.ByName("category"))
-	fmt.Fprintf(w, "READ ARTICLE, %s!\n", ps.ByName("article"))
-}
-
-func blogWrite(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-	fmt.Fprintf(w, "WRITE CATEGORY, %s!\n", ps.ByName("category"))
-	fmt.Fprintf(w, "WRITE ARTICLE, %s!\n", ps.ByName("article"))
-}
-
-func index(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-	err := tpl.ExecuteTemplate(w, "index.gohtml", nil)
-	handleError(w, err)
-}
-
-func about(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-	err := tpl.ExecuteTemplate(w, "about.gohtml", nil)
-	handleError(w, err)
-}
-
-func contact(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-	err := tpl.ExecuteTemplate(w, "contact.gohtml", nil)
-	handleError(w, err)
-}
-
-func apply(w http.ResponseWriter, req *http.Request, _ httprouter.Params) {
-	err := tpl.ExecuteTemplate(w, "apply.gohtml", nil)
-	handleError(w, err)
-}
-
-func applyProcess(w http.ResponseWriter, req *http.Request, _ httprouter.Params) {
-	err := tpl.ExecuteTemplate(w, "applyProcess.gohtml", nil)
-	handleError(w, err)
-}
-
 func main() {
-	mux := httprouter.New()
-	mux.GET("/", index)
-	mux.GET("/about", about)
-	mux.GET("/contact", contact)
-	mux.GET("/apply", apply)
-	mux.POST("/apply", applyProcess)
-	mux.GET("/user/:name", user)
-	mux.GET("/blog/:category/:article", blogRead)
-	mux.POST("/blog/:category/:article", blogWrite)
-	http.ListenAndServe("localhost:8080", mux)
+
+	http.Handle("/", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		data := "Index Page"
+		tpl.ExecuteTemplate(w, "index.html", struct {
+			Data string
+		}{
+			data,
+		})
+	}))
+
+	http.Handle("/dog/", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		data := "Dogs Page"
+		tpl.ExecuteTemplate(w, "index.html", struct {
+			Data string
+		}{
+			data,
+		})
+	}))
+
+	http.Handle("/me/", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		idx := strings.Replace(r.URL.Path, "/me/", "", 1)
+		data := fmt.Sprintf("My name is %s", idx)
+		tpl.ExecuteTemplate(w, "index.html", struct {
+			Data string
+		}{
+			data,
+		})
+	}))
+
+	http.ListenAndServe("localhost:8000", nil)
+
 }
