72d14aa65c4a1ada4a1f3ef4ad0485449ae7169a
quavious <leenowon97@gmail.com>
2020-11-13 01:21:35 +0900

    cookie

--- main.go
+++ main.go
@@ -9,41 +9,60 @@ import (
 
 var tpl *template.Template
 
-func init() {
-	tpl = template.Must(template.ParseGlob("templates/*"))
+func main() {
+	http.HandleFunc("/", writeCookie)
+	http.HandleFunc("/read", readCookie)
+	http.HandleFunc("/more", moreCookie)
+	http.Handle("/favicon.ico", http.NotFoundHandler())
+	http.ListenAndServe("localhost:8000", nil)
 }
 
-func foo(w http.ResponseWriter, r *http.Request) {
-	fmt.Println(r.Method)
+func writeCookie(w http.ResponseWriter, r *http.Request) {
+	http.SetCookie(w, &http.Cookie{
+		Name:  "MyCookie",
+		Value: "Chocolate Cookies",
+		Path:  "/",
+	})
+	fmt.Fprintf(w, "Cookie written\n")
+}
 
-	bs := make([]byte, r.ContentLength)
-	r.Body.Read(bs)
-	body := string(bs)
-
-	err := tpl.ExecuteTemplate(w, "index.gohtml", body)
+func readCookie(w http.ResponseWriter, r *http.Request) {
+	c1, err := r.Cookie("MyCookie")
 	if err != nil {
 		log.Println(err)
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
+	} else {
+		fmt.Fprintln(w, "Your Cookie 1 is : ", c1)
+	}
+
+	c2, err := r.Cookie("general")
+	if err != nil {
+		log.Println(err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	} else {
+		fmt.Fprintln(w, "Your Cookie 1 is : ", c2)
+	}
+
+	c3, err := r.Cookie("specific")
+	if err != nil {
+		log.Println(err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	} else {
+		fmt.Fprintln(w, "Your Cookie 1 is : ", c3)
 	}
 }
 
-func bar(w http.ResponseWriter, r *http.Request) {
-	log.Println("Your Request method at bar : ", r.Method)
-	//w.Header().Set("Location", "/barred")
-	//w.WriteHeader(http.StatusSeeOther)
-	http.Redirect(w, r, "/barred", http.StatusMovedPermanently)
-}
-
-func barred(w http.ResponseWriter, r *http.Request) {
-	log.Println("Your request method at barred : ", r.Method)
-	tpl.ExecuteTemplate(w, "index.gohtml", nil)
-}
-
-func main() {
-	http.HandleFunc("/", foo)
-	http.HandleFunc("/bar", bar)
-	http.HandleFunc("/barred", barred)
-	http.Handle("/favicon.ico", http.NotFoundHandler())
-	http.ListenAndServe("localhost:8000", nil)
+func moreCookie(w http.ResponseWriter, r *http.Request) {
+	http.SetCookie(w, &http.Cookie{
+		Name:  "general",
+		Value: "Some Other Cookie about general",
+	})
+	http.SetCookie(w, &http.Cookie{
+		Name:  "specific",
+		Value: "Some Other Cookie about specific",
+	})
+	fmt.Fprintln(w, "More cookies written")
 }
